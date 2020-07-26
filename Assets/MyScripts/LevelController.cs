f8fd51cf28ce7d27cf2f840212355d554757466c
alkihuri <alkihuri@gmail.com>
2020-07-26 13:04:50 +0300

    Второй коммит
    Доавлена базовая сцена. Пару скриптов для управления.

---
+++ Assets/MyScripts/LevelController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    GameObject Envrt;
+    float horizonta, vertical;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Envrt = GameObject.FindGameObjectWithTag("Environment");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizonta = Input.GetAxis("Mouse Y");
+        vertical = Input.GetAxis("Mouse X");
+        Envrt.transform.Rotate(horizonta, horizonta, 0);
+    }
+}
